a2f0154072518199f86c543fc7e3c47957eadb31
EliTheNicasio <ElijahN8@gmail.com>
2014-06-16 18:26:56 -0400

    Base files

---
+++ desktop/src/com/eNic/radar/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.eNic.radar.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.eNic.radar.RadarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RadarGame(), config);
+	}
+}
